62f4270df00397b1e3bb7243a4f758403d0a3d91
ghecko <ghecko78@gmail.com>
2020-06-30 15:34:15 +0200

    Adding git_init script

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.avrisp.read_fuses.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.avrisp.read_fuses.git
+git remote -v
